72e3c9ca9ba159a4d35ef4b95235f5401e80b9e6
sh <2662419405@qq.com>
2019-12-22 10:25:42 +0800

    提交

---
+++ githubfork.js
@@ -0,0 +1,2 @@
+var g = '<a class="githubfork" href="https://github.com/2662419405"><img width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1"></a>';
+    $("body").append(g);
