ce4a88e0e36ac187087580249911028d70d712cd
2662419405 <2662419405@qq.com>
2021-07-30 22:27:19 +0800

    博客园左上角头像

--- githubfork1.js
+++
@@ -1,2 +0,0 @@
-var g = '<a target="_blank" class="githubfork" href="https://github.com/2662419405"><img width="149" height="149" src="https://github.blog/wp-content/uploads/2008/12/forkme_right_darkblue_121621.png?resize=149%2C149" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1"></a>';
-    $("body").append(g);
---
+++ githubfork2.js
@@ -0,0 +1,2 @@
+var g = '<a target="_blank" class="githubfork" href="https://github.com/2662419405"><img width="149" height="149" src="https://raw.githubusercontent.com/2662419405/PicGo_Repository/master/forkme_right_darkblue_121621.png" class="attachment-full size-full" alt="Fork me on GitHub" data-recalc-dims="1"></a>';
+    $("body").append(g);
